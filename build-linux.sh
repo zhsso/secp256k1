dd0efd94ce9517ad850bb1b76c935467797e2ec8
Matt Little <zone117x@gmail.com>
2018-06-18 17:50:49 -0700

    Update windows build for 32bit and 64bit

--- build-linux.sh
+++ build-linux.sh
@@ -2,4 +2,4 @@
 
 rm -rf "build"
 cmake -H. -Bbuild
-cmake --build ./build --target secp256k1 --config RelWithDebInfo -- -j 4
+cmake --build ./build --target secp256k1 --config RelWithDebInfo
--- build-win.bat
+++ build-win.bat
@@ -1,3 +1,18 @@
-rd /s /q "build"
-cmake -H. -Bbuild -G "Visual Studio 15 2017 Win64"
-cmake --build ./build --target secp256k1 --config RelWithDebInfo
+@echo off
+
+if NOT "%1" == "x64" if NOT "%1" == "x86" (
+    echo Must specify first argument as x86 or x64
+    goto :error
+)
+if "%1" == "x64" (
+    set arch=64
+    set cmake_gen="Visual Studio 15 2017 Win64"
+)
+if "%1" == "x86" (
+    set arch=32
+    set cmake_gen="Visual Studio 15 2017"
+)
+
+rd /s /q "build_%arch%"
+cmake -H. -Bbuild_%arch% -G %cmake_gen%
+cmake --build ./build_%arch% --target secp256k1 --config RelWithDebInfo
